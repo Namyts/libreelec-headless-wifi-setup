2ac2cae809c8961bd6107f8fe12db3ac23199156
Namyts <35004248+Namyts@users.noreply.github.com>
2022-08-05 21:17:35 +0100

    added EVEN MORE logs

--- libreelec_wifi_setup.sh
+++ libreelec_wifi_setup.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
+echo "Loading SSID and password from the config files..."
 SSID=`cat /storage/wifi_ssid.txt`
 PASSWORD=`cat /storage/wifi_password.txt`
 
+echo "SSID:"
 echo "$SSID"
+echo "Password:"
 echo "$PASSWORD"
 
+echo "Enabling wifi..."
 connmanctl enable wifi
+echo "Scanning wifi..."
 connmanctl scan wifi
 
+echo "Results:"
 SERVICES=`connmanctl services`
 echo "$SERVICES"
 
@@ -16,8 +22,8 @@ HAS_WIFI_ALREADY=`echo "$SERVICES" | grep \*AO`
 
 if [ -z "$HAS_WIFI_ALREADY" ]
 then
-	rm */ -r
-	echo "Already connected to Wifi"
+	echo "Already connected to Wifi. Deleting previous config..."
+	rm /storage/.cache/connman/*/ -rf
 	exit 1
 else
 	CONNECTION=`echo "$SERVICES" | grep $SSID | head -n 1`
